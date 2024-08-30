84911375a7f0d82ab18ef9221d05425cfd8018fe
khazenor <80185732+khazenor@users.noreply.github.com>
2024-08-30 16:49:45 -0500

    requested delivered books

--- kubejs/startup_scripts/npc_food_purchase/item_book_generation.js
+++ kubejs/startup_scripts/npc_food_purchase/item_book_generation.js
@@ -1,3 +1,13 @@
+global.getOrderBookRows = (requestedIds, deliveredIds) => {
+  let rows = []
+  rows.push('=== REQUESTED ===')
+  rows = Array.prototype.concat(rows, global.generateItemRows(requestedIds))
+  rows.push('')
+  rows.push('=== DELIVERED ===')
+  rows = Array.prototype.concat(rows, global.generateItemRows(deliveredIds))
+  return rows
+}
+
 global.generateItemRows = (itemIds) => {
   let rows = []
   for (let itemId of itemIds) {
