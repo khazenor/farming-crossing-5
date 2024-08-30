58584845fc1f13bc60ad9e0e132358d0e9df3ffd
khazenor <80185732+khazenor@users.noreply.github.com>
2024-08-30 15:52:18 -0500

    written book generation

---
+++ kubejs/startup_scripts/lib/written_book_generation.js
@@ -0,0 +1,52 @@
+global.generateBookContent = (author, title, rows) => {
+  let outString = 'minecraft:written_book['
+  outString += 'written_book_content={'
+  outString += `author:"${author}",`
+  outString += 'pages:['
+  outString += global.getPagesContent(rows)
+  outString += '],' // pages
+  outString += 'resolved:1b,'
+  outString += `title:{raw:"${title}"}`
+  outString += '}' // written_book_content
+  outString += ']' // minecraft:written_book
+  
+  return outString
+}
+
+global.getPagesContent = (rows) => {
+  let pagesContent = ''
+  let pageArrays = global.getPageArrays(rows)
+  for (let i = 0; i<pageArrays.length; i++) {
+    let pageArray = pageArrays[i]
+    pagesContent += global.getPageContent(pageArray)
+    if (i < pageArrays.length - 1) {
+      pagesContent += ','
+    }
+  }
+  return pagesContent
+}
+
+global.getPageContent = (pageArray) => {
+  let pageContent = `{raw:'"`
+  
+  for (let row of pageArray) {
+    pageContent += row
+    pageContent += `\n`
+  }
+
+  pageContent += `"'}`
+  return pageContent
+}
+
+global.getPageArrays = (rows) => {
+  let numRowInPage = 14
+  let pages = [[]]
+  for (let row of rows) {
+    if (pages[pages.length - 1].length === numRowInPage) {
+      pages.push([row])
+    } else {
+      pages[pages.length - 1].push(row)
+    }
+  }
+  return pages
+}
