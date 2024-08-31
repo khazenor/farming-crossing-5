cf98f02b197cf0b321266de159ef830c3f1511dd
khazenor <80185732+khazenor@users.noreply.github.com>
2024-08-31 16:54:33 -0500

    written book only clear order if there is an order

--- kubejs/server_scripts/npc_food_purchase/right_click_written_book.js
+++ kubejs/server_scripts/npc_food_purchase/right_click_written_book.js
@@ -1,6 +1,10 @@
 ItemEvents.rightClicked('minecraft:written_book', event => {
   let player = event.player
-  if (player.shiftKeyDown && isItemCurrentOrderBook(player, event.item)) {
+  if (
+    player.shiftKeyDown &&
+    isItemCurrentOrderBook(player, event.item) &&
+    player.persistentData.activeOrder
+  ) {
     if (checkAreYouSure(player)) {
       playerTellTrans(player, 'npcFoodPurchase.currentOrderCleared')
       player.mainHandItem.count = 0
