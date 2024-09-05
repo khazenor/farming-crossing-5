cfccfd94ae8657663d5edd471630c0e7aa513242
khazenor <80185732+khazenor@users.noreply.github.com>
2024-09-04 19:15:25 -0500

    fish crafting table sell

--- $notes/ScriptGeneratorFC5/exportScripts.py
+++ $notes/ScriptGeneratorFC5/exportScripts.py
@@ -1,4 +1,6 @@
 from src import marketShopGen
+from src import fishCraftingTableSell
 
 if __name__ == "__main__":
+	fishCraftingTableSell.generateFishCraftingSellRecipes()
 	marketShopGen.generateMarketShops()
