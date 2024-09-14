38924a8211a7024290c4fb42de136b1657cd06e9
khazenor <80185732+khazenor@users.noreply.github.com>
2024-09-14 18:23:29 -0500

    refactor check are you sure

--- kubejs/server_scripts/lib/timing_funcs.js
+++ kubejs/server_scripts/lib/timing_funcs.js
@@ -1,15 +1,75 @@
+let timeLogByPlayer = {}
+
 const checkAreYouSure = (player) => {
-  let timeLimit = 10
-  let areYouSureTime = parseFloat(global.genStrFromObj(player.persistentData.areYouSureTime))
-  let curTime = getCurTime()
-  if (areYouSureTime && (curTime - areYouSureTime < timeLimit)) {
+  return checkAreYouSureLike(player, 'checkAreYouSure', 10)
+}
+
+const checkAreYouSureLike = (player, activityId, timeLimit) => {
+  if (lastActivityLessThan(player, activityId, timeLimit)) {
+    clearActivity(player, activityId)
     return true
   } else {
-    player.persistentData.areYouSureTime = `${curTime}`
     return false
   }
 }
 
+const lastActivityLessThan = (player, activityId, time) => {
+  let timeSince = timeSinceLastActivity(player, activityId)
+  return timeSince > 0 && timeSince <= time
+}
+
+const lastActivityMoreThan = (player, activityId, time) => {
+  let timeSince = timeSinceLastActivity(player, activityId)
+  return timeSince >= time
+}
+
+const timeSinceLastActivity = (player, activityId) => {
+  let curTime = getCurTime()
+  if (hasLastActivity(player, activityId)) {
+    let lastTime = getLastActivityTime(player, activityId)
+    logActivity(player, activityId)
+    return curTime - lastTime
+  } else {
+    logActivity(player, activityId)
+    return 0
+  }
+}
+
+const logActivity = (player, activityId) => {
+  setActivity(player, activityId, getCurTime())
+}
+
+const clearActivity = (player, activityId) => {
+  setActivity(player, activityId, null)
+}
+
+const setActivity = (player, activityId, value) => {
+  let playerName = player.name.getString()
+  if (!(playerName in timeLogByPlayer)) {
+    let addObj = {}
+    addObj[activityId] = value
+    timeLogByPlayer[playerName] = addObj
+  } else {
+    timeLogByPlayer[playerName][activityId] = value
+  }
+}
+
+const getLastActivityTime = (player, activityId) => {
+  let playerName = player.name.getString()
+  return timeLogByPlayer[playerName][activityId]
+}
+
+const hasLastActivity = (player, activityId) => {
+  let playerName = player.name.getString()
+  if (!timeLogByPlayer[playerName]) {
+    return false
+  } else if (!timeLogByPlayer[playerName][activityId]) {
+    return false
+  } else {
+    return true
+  }
+}
+
 const getCurTime = () => {
-  return Math.floor(new Date().getTime() / 1000)
+  return Number(new Date().getTime() / 1000)
 }
