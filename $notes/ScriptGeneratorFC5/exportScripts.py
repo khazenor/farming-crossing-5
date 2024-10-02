da5e378a0611aea64274e2cc54d94272b6af07b2
khazenor <80185732+khazenor@users.noreply.github.com>
2024-10-02 15:35:08 -0500

    remove music generation script for now

--- $notes/ScriptGeneratorFC5/exportScripts.py
+++ $notes/ScriptGeneratorFC5/exportScripts.py
@@ -3,10 +3,8 @@ from src import fishCraftingTableSell
 from src import floraDuplication
 from src import furnitureCutting
 from src import customVillagers
-from src import musicDiscs
 
 if __name__ == "__main__":
-	musicDiscs.deployMusicDiscs()
 	customVillagers.deployFunctions()
 	furnitureCutting.genFurnitureCuttingSupport()
 	floraDuplication.generateDuplicationRecipes()
