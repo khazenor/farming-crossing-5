749ea1ace24fe2981e28e926040d0680d90d7f69
khazenor <80185732+khazenor@users.noreply.github.com>
2024-10-05 23:33:12 -0500

    add dialog for villagers

--- kubejs/server_scripts/npc_mechanics/npc_dialog_definitions.js
+++ kubejs/server_scripts/npc_mechanics/npc_dialog_definitions.js
@@ -4,7 +4,23 @@ const npcInfo = (npcName, playerName) => {
       dialogs: [
         Text.translate("dialog.jessHi1", npcName),
         Text.translate("dialog.jessHi2Player", npcName, playerName),
-        Text.translate("dialog.jessHi3", npcName, playerName)
+        Text.translate("dialog.jessHi3", npcName, playerName),
+        Text.translate("dialog.jessHi4", npcName)
+      ]
+    },
+    "Pamela": {
+      dialogs: [
+        Text.translate("dialog.pamela1", npcName, playerName),
+        Text.translate("dialog.pamela2", npcName, playerName),
+        Text.translate("dialog.pamela3", npcName),
+        Text.translate("dialog.pamela4", npcName)
+      ]
+    },
+    "Sam": {
+      dialogs: [
+        Text.translate("dialog.sam1", npcName),
+        Text.translate("dialog.sam2", npcName),
+        Text.translate("dialog.sam3", npcName)
       ]
     }
   }
