c3296a745c7802f917c930ba16cd5810611f09e6
khazenor <80185732+khazenor@users.noreply.github.com>
2024-11-04 14:26:55 -0600

    quest translations

--- $notes/ScriptGeneratorFC5/lib/questTrans.py
+++ $notes/ScriptGeneratorFC5/lib/questTrans.py
@@ -1,5 +1,4 @@
 import os
-import ftb_snbt_lib
 
 questTransFolder = '..\\..\\config\\ftbquests\\quests\\lang'
 
@@ -16,23 +15,77 @@ def addTrans(transKey, transText, minecraftTransCode):
 
 def loadSnbt(minecraftTransCode):
 	fileLoc = questTransFileDir(minecraftTransCode)
+	dictTrans = {}
 	if os.path.exists(fileLoc):
-		return ftb_snbt_lib.load(
-			open(fileLoc, 'r', encoding='utf-8')
-		)
-	else:
-		return loadEmptySnbt()
+		lines = open(fileLoc, "r", encoding='utf-8').read().split('\n')
+		curIdx = 0
+		while curIdx < len(lines):
+			curIdx = parse(curIdx, lines, dictTrans)
+	return dictTrans
 
-def dumpSnbt(snbt, minecraftTransCode):
-	ftb_snbt_lib.dump(
-		snbt,
-		open(questTransFileDir(minecraftTransCode), "w", encoding='utf-8')
-	)
+def parse(curIdx, lines, dictTrans):
+	line = lines[curIdx]
+	if hasKey(line):
+		key = lineKey(line)
+		if (
+			hasArrayStart(line) and
+			hasArrayEnd(line)
+		):
+			dictTrans[key] = [lineText(line)]
+		elif hasText(line):
+			dictTrans[key] = lineText(line)
+		elif hasArrayStart(line):
+			curIdx += 1
+			arrLine = lines[curIdx]
+			textArray = []
+			while not hasArrayEnd(arrLine):
+				textArray.append(lineText(arrLine))
+				curIdx += 1
+				arrLine = lines[curIdx]
+			dictTrans[key] = textArray
+	return curIdx + 1
 
-def loadEmptySnbt():
-	enSnbt = ftb_snbt_lib.load(
-		open(questTransFileDir('en_us'), 'r', encoding='utf-8')
-	)
-	for key in list(enSnbt.keys()):
-		del enSnbt[key]
-	return enSnbt
+def hasKey(line):
+	return ":" in line
+
+def lineKey(line):
+	return line.split(":")[0].strip()
+
+def hasArrayStart(line):
+	return "[" in line
+
+def hasArrayEnd(line):
+	return "]" in line
+
+def hasText(line):
+	return '"' in line
+
+def lineText(line):
+	tempStr = '!@#$'
+	escapeStr = '\\"'
+	newLine = line.replace(escapeStr, tempStr)
+	value = newLine.split('"')[1]
+	value = value.replace(tempStr, escapeStr)
+	return value
+
+def dumpSnbt(transDict, minecraftTransCode):
+	with open(questTransFileDir(minecraftTransCode), "w", encoding='utf-8') as f:
+		f.write('{\n')
+		for key in transDict:
+			component = transDict[key]
+			if type(component) == str:
+				f.write(f'\t{key}: "{component}"\n')
+			elif type(component) == list:
+				f.write(f'\t{key}: [')
+				if len(component) > 1:
+					f.write('\n')
+				for element in component:
+					if len(component) > 1:
+						f.write('\t\t')
+					f.write(f'"{element}"')
+					if len(component) > 1:
+						f.write('\n')
+				if len(component) > 1:
+					f.write('\t')
+				f.write(f']\n')
+		f.write('}\n')
