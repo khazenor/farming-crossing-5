692ae38be897f440c03bacb563d338a7f3f11215
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-01 22:05:26 -0500

    remove copper nugget tags

--- kubejs/startup_scripts/src/event/event_handlers/tag_item_event_handler.js
+++ kubejs/startup_scripts/src/event/event_handlers/tag_item_event_handler.js
@@ -1,3 +1,15 @@
 const tagItemEventHandler = (event) => {
-  
+  tagItemEventHandlerHelper.removeTagsFromItems(event)
+}
+
+const tagItemEventHandlerHelper = {
+  removeTagsFromItems (event) {
+    let removeIds = [
+      'everythingcopper:copper_nugget'
+    ]
+
+    for (let removeId of removeIds) {
+      event.removeAllTagsFrom(removeId)
+    }
+  }
 }
