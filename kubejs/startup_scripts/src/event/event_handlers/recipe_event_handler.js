f8925511f37d9596f18134016cb8f537b3e48776
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-01 21:38:00 -0500

    remove logging

--- kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
+++ kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
@@ -2,8 +2,6 @@ const recipeEventHandler = (event) => {
   let shapelessRecipes = [].concat(
     CreateRecipes.shapelessRecipes()
   )
-  console.log('shapelessRecipes')
-  console.log(shapelessRecipes)
   for (let shapelessRecipe of shapelessRecipes) {
     EventWrapper.shapeless(event, shapelessRecipe[0], shapelessRecipe[1])
   }
