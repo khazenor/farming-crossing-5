c4897851eb9b6a107bdb449f512796c5d4d23ba4
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-01 21:48:09 -0500

    disable crafting for everythingcopper:copper_nugget

--- kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
+++ kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
@@ -1,6 +1,8 @@
 const recipeEventHandler = (event) => {
   recipeEventHandlerHelper.addShapelessRecipes(event)
+  recipeEventHandlerHelper.removeRecipes(event)
 }
+
 const recipeEventHandlerHelper = {
   addShapelessRecipes (event) {
     let shapelessRecipes = [].concat(
@@ -8,6 +10,30 @@ const recipeEventHandlerHelper = {
     )
     for (let shapelessRecipe of shapelessRecipes) {
       EventWrapper.shapeless(event, shapelessRecipe[0], shapelessRecipe[1])
+    }
+  },
+  removeRecipes (event) {
+    let recipeRemoval = {
+      ids: [
+        "advancednetherite:netherite_diamond_ingot",
+        "advancednetherite:netherite_emerald_ingot",
+        "advancednetherite:netherite_gold_ingot",
+        "advancednetherite:netherite_iron_ingot",
+        'everythingcopper:copper_nugget'
+      ],
+      mods: [
+        'progressiveflight',
+        'usefulhats',
+        'flightblocks'
+      ]
+    }
+
+    for (let id of recipeRemoval.ids) {
+      event.remove({ output: id })
+    }
+
+    for (let mod of recipeRemoval.mods) {
+      event.remove({ mod: mod })
     }
   }
 }
