af4af149f361c1ed8bc27fe025ff1a645778db19
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-01 21:40:46 -0500

    add recipe event handler helper

--- kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
+++ kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
@@ -1,8 +1,13 @@
 const recipeEventHandler = (event) => {
-  let shapelessRecipes = [].concat(
-    CreateRecipes.shapelessRecipes()
-  )
-  for (let shapelessRecipe of shapelessRecipes) {
-    EventWrapper.shapeless(event, shapelessRecipe[0], shapelessRecipe[1])
+  recipeEventHandlerHelper.addShapelessRecipes(event)
+}
+const recipeEventHandlerHelper = {
+  addShapelessRecipes (event) {
+    let shapelessRecipes = [].concat(
+      CreateRecipes.shapelessRecipes()
+    )
+    for (let shapelessRecipe of shapelessRecipes) {
+      EventWrapper.shapeless(event, shapelessRecipe[0], shapelessRecipe[1])
+    }
   }
 }
