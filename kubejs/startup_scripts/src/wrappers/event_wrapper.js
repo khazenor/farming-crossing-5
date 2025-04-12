795f1060eafcbdd49e0ff713c6e563feba1947b3
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-12 13:36:26 -0500

    add woodcutting and stonecutting even helpers

--- kubejs/startup_scripts/src/wrappers/event_wrapper.js
+++ kubejs/startup_scripts/src/wrappers/event_wrapper.js
@@ -1,5 +1,18 @@
 const EventWrapper = {
   shapeless (event, resultStr, ingArr) {
     event.shapeless(resultStr, ingArr)
+  },
+  stonecutting (event, outputId, inputTag, outputCount) {
+    if (!outputCount) {
+      outputCount = 1
+    }
+    event.stonecutting(`${outputCount}x ${outputId}`, `#${inputTag}`)
+  },
+  woodcutting (event, outputId, inputTag, outputCount) {
+    event.custom({
+      type: "corail_woodcutter:woodcutting",
+      ingredient: {tag: inputTag},
+      result: { id: outputId, count: outputCount }
+    })
   }
 }
