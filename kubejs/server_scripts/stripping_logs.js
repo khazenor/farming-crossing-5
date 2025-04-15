3e1118f28e7eedfb2eea0457c6248bd85497ddaf
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-14 22:09:30 -0500

    remove stripping recipes no longer in regions explored

--- kubejs/server_scripts/stripping_logs.js
+++ kubejs/server_scripts/stripping_logs.js
@@ -1,13 +1,9 @@
 ServerEvents.recipes(event => {
   let logsByMod = {
     "regions_unexplored": [
-      "alpha_log",
-      "ashen_log",
       "bamboo_log",
       "baobab_log",
       "blackwood_log",
-      "brimwood_log_magma",
-      "brimwood_log",
       "cobalt_log",
       "cypress_log",
       "dead_log",
@@ -21,7 +17,6 @@ ServerEvents.recipes(event => {
       "palm_log",
       "pine_log",
       "redwood_log",
-      "silver_birch_log",
       "small_oak_log",
       "socotra_log",
       "willow_log"
