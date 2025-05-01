f386a17d27c60389f3c7c681645498bad74b4dea
khazenor <80185732+khazenor@users.noreply.github.com>
2025-04-30 21:35:56 -0500

    dirt like fix

--- kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
+++ kubejs/startup_scripts/src/event/event_handlers/recipe_event_handler.js
@@ -9,7 +9,8 @@ const recipeEventHandlerHelper = {
     let shapelessRecipes = [].concat(
       CreateRecipes.shapelessRecipes(),
       FruitsDelightContainerFix.shapelessRecipes,
-      EverythingIsCopperFixes.shapelessRecipes
+      EverythingIsCopperFixes.shapelessRecipes,
+      DirtLikeFix.shapelessRecipes
     )
     for (let shapelessRecipe of shapelessRecipes) {
       EventWrapper.shapeless(event, shapelessRecipe[0], shapelessRecipe[1])
---
+++ kubejs/startup_scripts/src/features/dirt_like_fix.js
@@ -0,0 +1,22 @@
+const DirtLikeFix = {
+  dirtLikeItemIds: [
+    'minecraft:coarse_dirt',
+    'regions_unexplored:ashen_dirt',
+    'regions_unexplored:peat_coarse_dirt',
+    'regions_unexplored:peat_dirt',
+    'regions_unexplored:silt_coarse_dirt',
+    'regions_unexplored:silt_dirt'
+  ],
+  dirtId: 'minecraft:dirt',
+  get shapelessRecipes () {
+    let shapelessRecipes = []
+
+    for (let dirtLikeId of this.dirtLikeItemIds) {
+      shapelessRecipes.push([
+        this.dirtId,
+        [ dirtLikeId ]
+      ])
+    }
+    return shapelessRecipes
+  }
+}
