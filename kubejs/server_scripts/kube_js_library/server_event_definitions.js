9a747ae0c8caf763ea081aff725693bc292abdcc
khazenor <80185732+khazenor@users.noreply.github.com>
2025-05-14 10:55:45 -0500

    simplify RecipeViewerEventsAddInformationItem

--- kubejs/server_scripts/kube_js_library/server_event_definitions.js
+++ kubejs/server_scripts/kube_js_library/server_event_definitions.js
@@ -1,4 +1,3 @@
 RecipeViewerEvents.addInformation('item', event => {
-  global.eventWrappers.RecipeViewerEventsAddInformationItem(event)
-  // event.addItem('example:ingredient', ['Line 1', 'Line 2'])
+  global.RecipeViewerEventsAddInformationItem(event)
 })
--- kubejs/startup_scripts/kube_js_library/global_wappers.js
+++ kubejs/startup_scripts/kube_js_library/global_wappers.js
@@ -1,12 +1,11 @@
 // priority: -1
-global.eventWrappers = {
-  RecipeViewerEventsAddInformationItem (event) {
-    let requests = RequestHandler.jeiInfoForItems
-    for (let request of requests) {
-      event.add(request[0], request[1])
-    }
+global.RecipeViewerEventsAddInformationItem = (event) => {
+  let requests = RequestHandler.jeiInfoForItems
+  for (let request of requests) {
+    event.add(request[0], request[1])
   }
 }
+
 global.ItemEventsModifyTooltips = (event) => {
   RequestHandler.tooltips.forEach(request => {
     event.add(request[0], request[1])
