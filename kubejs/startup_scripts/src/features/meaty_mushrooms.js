f4c9c58956b9b440ff93d8451648fdc3b3d58f22
khazenor <80185732+khazenor@users.noreply.github.com>
2025-05-15 09:17:09 -0500

    requestify meaty mushroom item registration

---
+++ kubejs/startup_scripts/src/features/meaty_mushrooms.js
@@ -0,0 +1,9 @@
+RequestHandler.items.create.simpleFood([
+  // legacy items
+	['kubejs:roasted_brown_mushroom', 4, 1],
+	['kubejs:roasted_red_mushroom', 4, 1],
+
+  // current items
+	['kubejs:mushroom_patty', 2, 1],
+	['kubejs:cooked_mushroom_patty', 4, 1]
+])
