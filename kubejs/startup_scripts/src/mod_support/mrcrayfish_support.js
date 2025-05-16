c4ec9b6be50728a276c602a0244973254be84488
khazenor <80185732+khazenor@users.noreply.github.com>
2025-05-16 13:55:56 -0500

    Requestify mrcrayfish support

---
+++ kubejs/startup_scripts/src/mod_support/mrcrayfish_support.js
@@ -0,0 +1,31 @@
+// Mods interfaced:
+// - farmersdelight
+// - aquaculture
+
+RequestHandler.tags.item.add([
+  // salt for expanded delight
+  ['c:dusts/salt', [ 'refurbished_furniture:sea_salt' ]],
+
+  // knives for slicing
+  ['refurbished_furniture:tools/knives', [
+    "aquaculture:diamond_fillet_knife",
+    "aquaculture:gold_fillet_knife",
+    "aquaculture:iron_fillet_knife",
+    "aquaculture:neptunium_fillet_knife",
+    "aquaculture:stone_fillet_knife",
+    "aquaculture:wooden_fillet_knife",
+    "farmersdelight:diamond_knife",
+    "farmersdelight:flint_knife",
+    "farmersdelight:golden_knife",
+    "farmersdelight:iron_knife",
+    "farmersdelight:netherite_knife"
+  ]]
+])
+
+// compat recipes
+RequestHandler.recipes.add.shapeless([
+  ['refurbished_furniture:cheese', [
+    "refurbished_furniture:sea_salt",
+    "farmersdelight:milk_bottle"
+  ], 2]
+])
